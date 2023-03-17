684490f48354e24fa507f11045d6a31945b10c99
Douglas Maposa <doug.maposa@gmail.com>
2023-03-17 14:13:51 +0200

    Reformated users.js

--- server/app/routes/ecommerce/users.js
+++ server/app/routes/ecommerce/users.js
@@ -5,23 +5,23 @@ const router = express.Router();
 
 const User = require('../../models/ecommerce/User');
 
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     const userList = await User.find().select('-passwordHash');
 
-    if(!userList) {
-        res.status(500).json({success:false})
+    if (!userList) {
+        res.status(500).json({success: false})
     }
     res.send(userList);
 })
 
-router.get ('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const user = await User.findById(req.params.id).select('-passwordHash');
 
     if (!user) {
-        res.status(500).json({ success: false, message: 'The user with the given ID not exists' })
+        res.status(500).json({success: false, message: 'The user with the given ID not exists'})
     }
     res.status(200).send(user)
-    
+
 })
 
 router.post('/register', async (req, res) => {
@@ -48,28 +48,28 @@ router.post('/register', async (req, res) => {
 router.delete('/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id).then(user => {
         if (user) {
-            return res.status(200).json({ success: true, message: 'User deleted successfully' })
+            return res.status(200).json({success: true, message: 'User deleted successfully'})
         } else {
-            return res.status(404).json({ success: false, message: 'User cannot find' })
+            return res.status(404).json({success: false, message: 'User cannot find'})
         }
     }).catch(err => {
-        return res.status(400).json({ success: false, error: err })
+        return res.status(400).json({success: false, error: err})
     })
 })
 
 router.post('/login', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email})
+    const user = await User.findOne({email: req.body.email})
     const secret = process.env.secret;
 
-    if(!user) {
+    if (!user) {
         return res.status(400).send('User with given Email not found');
     }
 
-    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign({
             userID: user.id,
-            isAdmin : user.isAdmin
-        }, secret, {expiresIn : '1d'} )
+            isAdmin: user.isAdmin
+        }, secret, {expiresIn: '1d'})
         res.status(200).send({user: user.email, token: token});
     } else {
         res.status(400).send('Password is mismatch');
@@ -81,7 +81,7 @@ router.post('/login', async (req, res) => {
 router.get('/get/count', async (req, res) => {
     const userCount = await User.countDocuments((count) => count);
     if (!userCount) {
-        res.status(500), json({ success: false })
+        res.status(500), json({success: false})
     }
     res.status(200).send({
         userCount: userCount
