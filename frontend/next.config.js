d36e1e86d9c239168e24d99e50ab68c0d0e1ca64
Douglas Maposa <doug.maposa@gmail.com>
2023-04-12 15:10:57 +0200

    Clean file code format, favour space inplace of tabs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,7 +11,7 @@ module.exports = withBundleAnalyzer({
   trailingSlash: true,
   basePath: '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
+  // So, the source code is 'basePath-ready'.
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
